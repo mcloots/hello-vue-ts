7200aa0bd4c4ad7fbb0bf8cd9668e0ee6b88bd72
mcloots <michael.cloots@thomasmore.be>
2020-04-24 15:40:59 +0200

    logout + nav items based on authentication state

---
+++ src/components/Authentication/Logout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h2>Logging out</h2>
+  </div>
+</template>
+
+<script lang="ts">
+//Documentation decorators: https://github.com/kaorun343/vue-property-decorator
+import { Component, Prop, Watch, Vue } from "vue-property-decorator";
+import firebase from "firebase";
+
+
+//const userModule = namespace('User');
+
+@Component({
+  //name: "login"
+})
+export default class Logout extends Vue {
+  mounted() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.replace({
+            name: "Home"
+          });
+        });
+    }
+}
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -42,6 +42,15 @@ const routes: Array<RouteConfig> = [
     component: () => import(/* webpackChunkName: "about" */ '@/components/Authentication/LoginRegister.vue')
   },
   {
+    path: '/logout',
+    name: 'Logout',
+    meta: { title: 'Logout' },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '@/components/Authentication/Logout.vue')
+  },
+  {
     path: '/dashboard',
     name: 'Dashboard',
     component: Dashboard,
