a92ba7838413abc1c725541e4c8f3e249ae14330
mcloots <michael.cloots@thomasmore.be>
2020-04-24 10:43:08 +0200

    navbar

--- src/components/Navigation/NavBar.vue
+++ src/components/Navigation/NavBar.vue
@@ -1,13 +1,21 @@
 <template>
     <b-navbar toggleable="md" type="dark" variant="info">
+    <b-navbar-brand href="#">Hello Vue TS
+      <!-- <img :src="require('../../assets/logo.png')" id='logo'> Meal App -->
+    </b-navbar-brand>
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-    <!-- <b-navbar-brand>
-      <img :src="require('../../assets/logo.png')" id='logo'> Meal App
-    </b-navbar-brand> -->
     <b-collapse is-nav id="nav_collapse">
       <b-navbar-nav>
         <b-nav-item :to="{ name: 'Home'}" :active="$route.name == 'Home'">Home</b-nav-item>
         <b-nav-item :to="{ name: 'Login'}" :active="$route.name == 'Login'">Login</b-nav-item>
+        <b-nav-item-dropdown text="Categories" right>
+          <b-dropdown-item>
+            Test 1
+          </b-dropdown-item>
+          <b-dropdown-item>
+            Test 2
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
         <!-- <b-nav-item :to="{ name: 'random'}" :active="$route.name.includes('random')">Random</b-nav-item>
         <b-nav-item :to="{ name: 'search'}" :active="$route.name.includes('search')">Search</b-nav-item> -->
         <!-- <b-nav-item-dropdown text="Categories" right>
