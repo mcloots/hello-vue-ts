c78a4b2495f7c28b9ab6924123c536a8dada5702
mcloots <michael.cloots@thomasmore.be>
2020-04-26 16:31:44 +0200

    v-for, v-if, v-show, reactive property

--- src/components/Timeline/Timeline.vue
+++ src/components/Timeline/Timeline.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="container">
     <h2>Timeline</h2>
-    <div v-if="user" class="alert alert-success" role="alert">You are logged in!</div>
-    <h3>{{ user }}</h3>
+    <!-- <div v-if="user" class="alert alert-success" role="alert">You are logged in!</div>
+    <h3>{{ user }}</h3> -->
+    <ul>
+        <li v-for="com in communicationArray" :key="com.id">
+            <a @click="selectedComm = com">{{com.message}}</a>
+        </li>
+    </ul>
+    <!-- v-if: added er removed from DOM -->
+    <div v-if="selectedComm">{{ selectedComm.message }}</div>
+    <!-- Show/hide, but always in DOM : display:none -->
+    <div v-if="selectedComm">{{ selectedComm.message }}</div>
+    <div>
+        <label for="show">
+            Show more
+            <input type="checkbox" id="show" v-model="showMore" />
+        </label>
+    </div>
+    <p v-show="showMore">Show me!</p>
   </div>
 </template>
 <script lang="ts">
@@ -14,6 +30,8 @@ import store from "@/store";
 @Component
 export default class Timeline extends Vue {
   user = this.$store.state.myUser.userM;
+  selectedComm: Communication | null = null;
+  showMore: boolean = false;
 
   communicationArray: Communication[] = [
     { id: 1, message: "Eerste bericht" },
