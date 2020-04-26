c2e5771c4ca592380a6774f7c6662565c92df041
mcloots <michael.cloots@thomasmore.be>
2020-04-26 22:56:12 +0200

    bugfix login (unused property user)

--- src/components/Authentication/LoginRegister.vue
+++ src/components/Authentication/LoginRegister.vue
@@ -3,7 +3,7 @@
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card">
-          <div class="card-header">{{formTitle}} {{user}}</div>
+          <div class="card-header">{{formTitle}}</div>
           <div class="card-body">
             <div v-if="error" class="alert alert-danger">{{error}}</div>
             <form action="#" @submit.prevent="submit">
